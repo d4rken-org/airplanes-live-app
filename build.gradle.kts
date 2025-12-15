ecc2dde4a4ae3f7568dfbc9dfd52435e5a71d571
darken <darken@darken.eu>
2025-12-15 15:39:08 +0100

    Update Android Gradle Plugin to 8.13.2
    Bump `com.android.tools.build:gradle` from `8.11.0` to `8.13.2` in both the root build script and `buildSrc`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.11.0")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
         classpath("org.jetbrains.kotlin:kotlin-serialization:2.2.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.56.2")
