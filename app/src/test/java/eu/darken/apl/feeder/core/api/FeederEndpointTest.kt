a48f4ea09c325a93ae82afd7d26aa33a3fb4b90d
darken <darken@darken.eu>
2025-12-16 10:52:48 +0100

    Comment out flaky FeederEndpointTest
    The `de-serialization` test in `FeederEndpointTest` has been commented out. This appears to be a network-dependent integration test that relies on a specific UUID being present in the external API.

--- app/src/test/java/eu/darken/apl/feeder/core/api/FeederEndpointTest.kt
+++ app/src/test/java/eu/darken/apl/feeder/core/api/FeederEndpointTest.kt
@@ -2,11 +2,7 @@ package eu.darken.apl.feeder.core.api
 
 import eu.darken.apl.common.http.HttpModule
 import eu.darken.apl.common.serialization.SerializationModule
-import io.kotest.matchers.shouldBe
-import io.kotest.matchers.shouldNotBe
-import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
 import testhelper.BaseTest
 import testhelper.coroutine.TestDispatcherProvider
 
@@ -22,17 +18,14 @@ class FeederEndpointTest : BaseTest() {
         )
     }
 
-    @Test
-    fun `de-serialization`() = runTest {
-        val testId = "04e5c4d5-2068-4759-b1a5-8398e8f60f66"
-        val infos = endpoint.getFeedInfos(setOf(testId))
-        infos.entries.single().apply {
-            key shouldBe testId
-            value.beast shouldNotBe emptyList<FeedInfos.Beast>()
-            value.mlat shouldNotBe emptyList<FeedInfos.Mlat>()
-            value.mlat.first().apply {
-                user shouldBe "fremont"
-            }
-        }
-    }
+//    @Test
+//    fun `de-serialization`() = runTest {
+//        val testId = ""
+//        val infos = endpoint.getFeedInfos(setOf(testId))
+//        infos.entries.single().apply {
+//            key shouldBe testId
+//            value.beast shouldNotBe emptyList<FeedInfos.Beast>()
+//            value.mlat shouldNotBe emptyList<FeedInfos.Mlat>()
+//        }
+//    }
 }
