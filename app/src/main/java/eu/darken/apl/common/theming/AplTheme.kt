9b207d47516bfd8b183ad77436e5084aaf472c44
darken <darken@darken.eu>
2026-03-09 16:22:55 +0100

    UI: Fix white flash during screen transitions in dark mode

--- app/src/main/java/eu/darken/apl/common/theming/AplTheme.kt
+++ app/src/main/java/eu/darken/apl/common/theming/AplTheme.kt
@@ -1,6 +1,7 @@
 package eu.darken.apl.common.theming
 
 import android.app.Activity
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -46,6 +48,7 @@ fun AplTheme(
             val insetsController = WindowCompat.getInsetsController(window, window.decorView)
             insetsController.isAppearanceLightStatusBars = !darkTheme
             insetsController.isAppearanceLightNavigationBars = !darkTheme
+            window.setBackgroundDrawable(ColorDrawable(colorScheme.background.toArgb()))
         }
     }
 
